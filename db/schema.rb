bf53972b921715d91b7f24fafbcb22d5a7ae2bc4
Osshrp <osshrp@gmail.com>
2017-08-19 16:11:31 +0300

    add some specs

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(version: 20170819084804) do
   create_table "calendars", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_calendars_on_user_id"
   end
 
   create_table "calendars_events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
